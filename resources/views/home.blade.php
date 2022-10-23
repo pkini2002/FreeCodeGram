a533ea13430826edb6ce4cfaa554e3f35e1eb29f
PrathikshaKini <kiniprathiksha18@gmail.com>
2022-10-23 20:54:09 +0530

    UI of home page added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,38 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="svg/insta-pic.png" alt="" style="height:150px;">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>freecodecamp</h1></div>
+            <div class="d-flex">
+                <div style="padding-right:25px;"><strong>153 </strong>posts</div>
+                <div style="padding-right:25px;"><strong>23k </strong>followers</div>
+                <div style="padding-right:25px;"><strong>212 </strong>following</div>
             </div>
+            <div class="pt-4" style="font-weight:bold;">freeCodeCamp.org</div>
+            <div>We're a global community of millions of people learning to code together.
+LearnToCodeRPG: https://www.freecodecamp.org/news/learn-to-code-rpg/</div>
+            <div><a href="https://www.freecodecamp.org/" style="text-decoration:none;color:blue;">www.freecodecamp.org</a></div>
         </div>
     </div>
+
+<div class="row" style="padding-top:30px;">
+    <div class="col-4">
+        <img src="svg/post1.png" style="width:300px;" alt="">
+    </div>
+    <div class="col-4">
+        <img src="svg/post-2.png" style="width:300px;" alt="">
+    </div>
+    <div class="col-4">
+        <img src="svg/post-3.png" style="width:300px;" alt="">
+    </div>
 </div>
+
+
+</div>
+
+
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,8 +20,9 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand d-flex" href="{{ url('/') }}">
+                    <div><img src="/svg/freeCodeCamp.png" alt="" style="height:25px;"></div>
+                    <div class="pl-3" style="padding-left:20px;"> FreeCodeGram </div>
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
