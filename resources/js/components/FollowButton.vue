694fcb5d42a1a7c10d7d1b4b32ca4cb3e3132a8b
PrathikshaKini <kiniprathiksha18@gmail.com>
2022-10-26 09:57:08 +0530

    changes

---
+++ resources/js/components/FollowButton.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <button class="btn btn-primary" style="width:100px;height:40px;">Follow</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
